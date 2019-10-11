316876e843718387a2688a7efbec5d4ab9b3cea9
Varshneya Rao <varevarao@gmail.com>
2019-10-11 20:19:25 +0530

    JSON editor plugin: Fixed support for recursive schema generation.

--- IDE/src/components/editor/plugins/json-schemas/index.js
+++ IDE/src/components/editor/plugins/json-schemas/index.js
@@ -1,49 +1,63 @@
 import ApplicationSchema from "./application-schema";
 
 const SCHEMA_MAP_FILE_TYPE = {
+    // Any predefined schemas mapped to file-type can be added here
     'application': ApplicationSchema
 }
 
 const SCHEMA_MAP_FILE_NAME = {
-    // 'kitsune-settings.json': ApplicationSchema
+    // Any predefined schemas mapped to a file-name can be added here
 }
 
+/**
+ * Recursively iterates over any valid JSON object
+ * and creates a schema for the same
+ * 
+ * @param {any} root The current root node
+ */
 const generateSchemaRecursive = (root) => {
-    const result = {};
+    const result = { type: '', properties: {}, items: [] };
 
     if (typeof root === 'object') {
-        for (let [key, value] of Object.entries(root)) {
-            result[key] = {
-                title: key,
-                type: value === null ? 'string' : Array.isArray(value) ? 'array' : (typeof value)
-            };
-
-            if (result[key].type === 'object') {
-                result[key].properties = generateSchemaRecursive(value);
-            }
-
-            if (result[key].type === 'array') {
-                result[key].items = [];
-                for (let item of value) {
-                    result[key].items.push(generateSchemaRecursive(item));
-                }
-            }
+        const isArray = Array.isArray(root);
+        // Mod the result based on type
+        if (isArray) {
+            result.type = 'array';
+            delete result.properties;
+        } else {
+            result.type = 'object';
+            delete result.items;
         }
 
-        return result;
-    } else {
-        return { type: Array.isArray(root) ? 'array' : typeof (root || 'string') };
+        // Iterate over object/array entries
+        for (let [key, value] of Object.entries(root)) {
+            const iterator = isArray ? result.items : result.properties;
+
+            // Recursively handle the value
+            iterator[key] = generateSchemaRecursive(value);
+
+            // If we're not in an array, set the title
+            if (!isArray) {
+                iterator[key].title = key;
+            }
+        }
     }
+    // Must be a primitive otherwise
+    else {
+        result.type = typeof (root || 'string');
+        delete result.properties;
+        delete result.items;
+    }
+
+    return result;
 }
 
 const getJSONSchema = (fileName, fileType, fileData) => {
     if (fileType in SCHEMA_MAP_FILE_TYPE) return SCHEMA_MAP_FILE_TYPE[fileType];
     else if (fileName in SCHEMA_MAP_FILE_NAME) return SCHEMA_MAP_FILE_NAME[fileName];
     else {
-        // Use fileData to build a schema
-        const schema = { title: '', type: 'object' };
-        schema.properties = generateSchemaRecursive(fileData);
-
+        // Use fileData to build a schema, with an empty top level display name
+        const schema = generateSchemaRecursive(fileData);
         return schema;
     }
 }
